00cce169abc651c917c0de704786c09516d4d5fc
Bob <cwmbranmathstutor@gmail.com>
2017-06-12 13:53:42 -0400

    first version of timer app

---
+++ timer.py
@@ -0,0 +1,17 @@
+import time
+import subprocess
+
+def timer():
+	timeleft = 3
+	while timeleft > 0:
+		print("timeleft = ", timeleft)
+		time.sleep(1)
+		timeleft -= 1
+		
+def buzzer():
+    while True:
+        subprocess.call(["aplay", "/usr/lib/libreoffice/share/gallery/sounds/train.wav"])
+        time.sleep(0.5)
+
+timer()
+buzzer()	
