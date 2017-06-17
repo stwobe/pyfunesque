0d6e7d58f6a77d8b868e875c474453015da6e6e7
stwobe <cwmbranmathstutor@gmail.com>
2017-06-17 18:26:48 +0100

    User input function add, allowing variable minutes

--- timer.py
+++ timer.py
@@ -13,9 +13,12 @@ See ReadMe file for which other programs you may need to run this
 import time # see below - this is used pause/count the seconds
 import subprocess # this is for calling commands from Bash - see below
 
+t = int(input("How many minutes?"))
+t = (t*60)
+
 def timer(): # our main timer function
 	subprocess.call(["aplay", "/usr/lib/libreoffice/share/gallery/sounds/horse.wav"])
-	timeleft = 300 #default is 10 minutes = 600 seconds - warm up oven
+	timeleft = (t) #default is 10 minutes = 600 seconds - warm up oven
 	while timeleft > 0: # set up while loop for timer
 		print("timeleft ", timeleft) #print how many seconds are left
 		time.sleep(1) # halt the program for one second on each pass
